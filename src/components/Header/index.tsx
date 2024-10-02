4f2fc492b7fde6bc9e5a1356d783315a424a85de
Linda Hrdinová <hrlin@seznam.cz>
2024-10-02 21:24:11 +0200

    header -

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,5 @@ import './styleHeader.css';
 import japonstinaSAjouHeader from './img/IMG_1323.jpg';
 
 export const Header: React.FC = () => {
-  return (
-    <header>
-      <img src={japonstinaSAjouHeader} alt="Ája" className="headerImg" />
-    </header>
-  );
+  return <header></header>;
 };
