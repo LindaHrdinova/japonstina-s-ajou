4eb747be1f94d778ec6ff595bacb63944fcc900c
Linda Hrdinová <hrlin@seznam.cz>
2024-12-10 22:52:40 +0100

    Font - Plus Jakarta Sans

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -38,28 +38,28 @@ export const Navigation: React.FC = () => {
         <ul className="menu__list">
           <a className="menuItem" href="/">
             <div className="menuItem">
-              <li>domů</li>
+              <li>Domů</li>
             </div>
           </a>
 
           <a className="menuItem" href="o-mne">
             <div className="menuItem">
-              <li>o mně</li>
+              <li>O mně</li>
             </div>
           </a>
           <a className="menuItem" href="kurzy">
             <div className="menuItem">
-              <li>kurzy</li>
+              <li>Kurzy</li>
             </div>
           </a>
           <a className="menuItem" href="kontakty">
             <div className="menuItem">
-              <li>kontakt</li>
+              <li>Kontakt</li>
             </div>
           </a>
           <a className="menuItem" href="https://www.fler.cz/japonstina-s-ajou">
             <div className="menuItem">
-              <li>e-shop</li>
+              <li>e-Shop</li>
             </div>
           </a>
         </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ const App: React.FC = () => {
       <Header />
       <Navigation />
       <main>
-        <div>
+        <div className="mainDiv">
           <Outlet />
         </div>
       </main>
