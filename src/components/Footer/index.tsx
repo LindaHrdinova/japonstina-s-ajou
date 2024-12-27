655517a3b52dac7b821d22c7b3995e81ac9d019d
Linda Hrdinová <hrlin@seznam.cz>
2024-12-28 00:54:24 +0100

    footer text-align

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,12 +3,12 @@ import './styleFooter.css';
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <span>
+      <span className="leftText">
         <a href="." className="footer_link">
           JAPONŠTINA S ÁJOU
         </a>
       </span>
-      <span>
+      <span className="rightText">
         <a href="obchodni-podminky" className="footer_link">
           Obchodní podmínky
         </a>
